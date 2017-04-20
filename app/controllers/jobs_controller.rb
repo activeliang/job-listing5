fa02736d1dd48ae417031a3eb6635c59da952777
Lotus <lvdeshi2011@gmail.com>
2017-04-20 16:28:52 +0800

    first version

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,55 +1,67 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user!, except:[:show, :index]
   before_action :validate_search_key, only: [:search]
-    def index
-      @jobs = Job.all
-    end
-    def show
-      @job = Job.find(params[:id])
 
-      if @job.is_hidden
+  def search
+    if @query_string.present?
+      search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
+      @jobs = search_result.recent.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
+  def index
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.where(:is_hidden=>false).order("wage_lower_bound DESC")
+    when "by_upper_bound"
+      Job.where(:is_hidden=>false).order("wage_upper_bound DESC")
+    else
+    Job.where(:is_hidden=>false).order("created_at DESC")
+    end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+
+    if @job.is_hidden
       flash[:warning] = "This Job already archieved"
       redirect_to root_path
     end
-    end
-    def new
-      @job = Job.new
-    end
-    def create
-      @job = Job.new(job_params)
-      if @job.save
-        redirect_to jobs_path
-      else
-        render :new
-      end
-    end
-    def edit
-      @job = Job.find(params[:id])
-    end
-    def update
-      @job = Job.find(params[:id])
-        if @job.update(job_params)
-          redirect_to jobs_path
-        else
-          render :new
-        end
-    end
-    def destroy
-      @job = Job.find(params[:id])
-      @job.destroy
+  end
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jobs_params)
+    if @job.save
       redirect_to jobs_path
+    else
+      render :new
     end
-    def search
-      if @query_string.present?
-        search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
-        @jobs = search_result.recent.paginate(:page => params[:page], :per_page => 5 )
-      end
-   end
-private
-    def job_params
-      params.require(:job).permit(:title,:description,:wage_upper_bound,:wage_lower_bound,:contact_email,:is_hidden)
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(jobs_params)
+      redirect_to jobs_path
+    else
+      render :edit
     end
-    def validate_search_key
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  protected
+
+  def validate_search_key
     @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
     if params[:q].present?
       @search_criteria =  {
@@ -58,20 +70,13 @@ private
     end
   end
 
+
   def search_criteria(query_string)
     { :title_cont => query_string }
   end
-  protected
-    def validate_search_key
-      @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
-      if params[:q].present?
-        @search_criteria =  {
-          title_or_company_or_city_cont: @query_string
-        }
-      end
-    end
 
-    def search_criteria(query_string)
-      { :title_cont => query_string }
-    end
+  private
+  def jobs_params
+    params.require(:job).permit(:title,:description,:wage_upper_bound,:wage_lower_bound,:contact_email,:is_hidden,:category,:company,:city)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :admin do
-    resources :jobs do
-      member do
-        post :publish
-        post :hide
-      end
-      resources :resumes
-    end
-  end
+namespace :admin do
   resources :jobs do
+    member do
+      post :publish
+      post :hide
+    end
+
     resources :resumes
   end
-  resources :welcome do
+end
+
+namespace :account do
+  resources :jobs
+end
+
+resources :jobs do
+  collection do
+    get :search
   end
-  resources :jobs do
-    collection do
-      get :search
-    end
-  end
-  root 'welcome#index'
+resources :resumes
+end
+   root 'welcome#index'
 end
